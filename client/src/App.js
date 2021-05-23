ff8640647f2bb7fd133a997a4562eba7b9f217fa
Nippun Tyagi <nippuntyagi@gmail.com>
2021-05-23 16:13:21 +0530

    initiated a react app and implemented 2 routes

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import { UserContext } from './userContext';
+import Chat from './components/chat/Chat';
+import Home from './components/home/Home';
+
+
+function App() {
+  const [user, setUser] = useState(null);
+  return (
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{user, setUser}}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/chat" component={Chat} />
+          </Switch>
+        </UserContext.Provider>
+      </div>  
+    </Router>
+  );
+}
+
+export default App;
